fe3da50be022328534169b04961a92ee79345beb
David Pulido <davidpulido1232019@gmail.com>
2024-04-01 13:25:44 -0500

    ejercicio 11

---
+++ assets/ejercicioshtml/javascript/ejercicio11.js
@@ -0,0 +1,12 @@
+let numero1 = parseFloat(prompt("Ingrese el primer número:"));
+let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+
+let suma = numero1 + numero2;
+let resta = numero1 - numero2;
+let multiplicacion = numero1 * numero2;
+let division = numero1 / numero2;
+
+alert("Suma: " + suma);
+alert("Resta: " + resta);
+alert("Multiplicación: " + multiplicacion);
+alert("División: " + division);
