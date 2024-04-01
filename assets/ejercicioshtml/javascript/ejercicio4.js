8fa8ca9661ce9e04455d1aafd352cdde73fdf826
David Pulido <davidpulido1232019@gmail.com>
2024-04-01 12:42:31 -0500

    ejercicio 4

---
+++ assets/ejercicioshtml/javascript/ejercicio4.js
@@ -0,0 +1,8 @@
+
+let numero = parseInt(prompt("Ingrese un número entero:"));
+
+if (numero % 2 === 0) {
+    alert("El número ingresado es par.");
+} else {
+    alert("El número ingresado es impar.");
+}
