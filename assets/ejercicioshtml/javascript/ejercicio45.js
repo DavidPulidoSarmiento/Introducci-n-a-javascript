ca120e35135723c490e5d84919afb6b53d920df7
David Pulido <davidpulido1232019@gmail.com>
2024-04-02 10:26:35 -0500

    ejercicio 45

---
+++ assets/ejercicioshtml/javascript/ejercicio45.js
@@ -0,0 +1,7 @@
+var numero = parseFloat(prompt("Ingrese un número:"));
+
+if (numero > 0 && numero < 100) {
+    console.log("El número ingresado es positivo y menor que 100.");
+} else {
+    console.log("El número ingresado no cumple con las condiciones requeridas.");
+}
