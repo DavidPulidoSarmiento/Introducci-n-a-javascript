ec9911cb1764e75401c47b30b57abb8a4e259dd0
David Pulido <davidpulido1232019@gmail.com>
2024-04-02 10:08:37 -0500

    ejercicio 35

---
+++ assets/ejercicioshtml/javascript/ejercicio35.js
@@ -0,0 +1,11 @@
+let gastoJuegosLlaves = 5 * 11500;
+let gastoBomba = 1168000;
+let gastoCajasPernos = 3 * 87000;
+
+let totalGastos = gastoJuegosLlaves + gastoBomba + gastoCajasPernos;
+
+let dineroSobrante = 91000;
+
+let dineroInicial = totalGastos + dineroSobrante;
+
+alert("El jefe de obra inicialmente tenía: $" + dineroInicial);
