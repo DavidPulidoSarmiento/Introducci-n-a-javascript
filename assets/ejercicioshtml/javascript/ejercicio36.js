fb32ab14763061855e093b878d52458acd742a56
David Pulido <davidpulido1232019@gmail.com>
2024-04-02 10:09:43 -0500

    ejercicio 36

---
+++ assets/ejercicioshtml/javascript/ejercicio36.js
@@ -0,0 +1,5 @@
+let cacaoDeseado = 20;  
+let proporcionCacaoReceta = 10; 
+let harinaNecesaria = (cacaoDeseado * 100) / proporcionCacaoReceta;
+
+alert("La cantidad de gramos de harina para hacer el bizcocho es: " + harinaNecesaria);
