5db88efb7663976417132aa3966c0c600d332a25
David Pulido <davidpulido1232019@gmail.com>
2024-04-02 09:10:19 -0500

    ejercicio 13

---
+++ assets/ejercicioshtml/javascript/ejercicio12.js
@@ -0,0 +1,6 @@
+let base = parseFloat(prompt("Ingrese la base del rectángulo:"));
+let altura = parseFloat(prompt("Ingrese la altura del rectángulo:"));
+
+let area = base * altura;
+
+alert("El área del rectángulo es: " + area);
---
+++ assets/ejercicioshtml/javascript/ejercicio13.js
@@ -0,0 +1,8 @@
+let numero1 = parseFloat(prompt("Ingrese el primer número:"));
+let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+let numero3 = parseFloat(prompt("Ingrese el tercer número:"));
+let numero4 = parseFloat(prompt("Ingrese el cuarto número:"));
+
+let media = (numero1 + numero2 + numero3 + numero4) / 4;
+
+alert("La media de los números ingresados es: " + media);
