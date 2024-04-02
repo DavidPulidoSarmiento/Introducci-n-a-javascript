c897743368b9db0fb054f0b3c83694e186269291
David Pulido <davidpulido1232019@gmail.com>
2024-04-02 09:38:26 -0500

    ejercicio 26

---
+++ assets/ejercicioshtml/javascript/ejercicio26.js
@@ -0,0 +1,5 @@
+let temperaturaCelsius = parseFloat(prompt("Ingrese la temperatura en grados Celsius:"));
+
+let temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
+
+alert("La temperatura en grados Fahrenheit es: " + temperaturaFahrenheit.toFixed(2));
