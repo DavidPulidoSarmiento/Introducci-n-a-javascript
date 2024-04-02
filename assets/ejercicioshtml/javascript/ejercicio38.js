bc915ac64766a813c1a82dbf4226914fddcf7de6
David Pulido <davidpulido1232019@gmail.com>
2024-04-02 10:15:54 -0500

    ejercicio 38

---
+++ assets/ejercicioshtml/javascript/ejercicio38.js
@@ -0,0 +1,6 @@
+let parteMartin = 2 / 3;
+let parteJairo = 1 / 4;
+let totalPartesMartinyJairo = parteMartin + parteJairo;
+let parteLorena = 1 - totalPartesMartinyJairo;
+
+alert("La parte que le corresponde a Lorena es: " + parteLorena.toFixed(2));
